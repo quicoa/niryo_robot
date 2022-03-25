1a6cc312cd1fe805c11df133f6120af1934470c2
Gerard Harkema <ga.harkema@avans.nl>
2022-03-25 12:31:16 +0100

    modified installation script, made relative

--- install/install.bash
+++ install/install.bash
@@ -2,9 +2,10 @@
 
 
 sudo apt install sshpass
-cd $HOME/niryo_robot_ws/src
+#cd $HOME/niryo_robot_ws/src
+cd ../..
 git  clone https://github.com/NiryoRobotics/ned_ros.git
-
-cd $HOME/niryo_robot_ws/
+#cd $HOME/niryo_robot_ws/
+cd ..
 catkin b
 
