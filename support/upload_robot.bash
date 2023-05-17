2eb3f9f617a4dae6d1b039947da360b7145dfb92
Student <student@avans.nl>
2023-05-17 03:11:11 -0700

    20230517

--- support/upload_robot.bash
+++ support/upload_robot.bash
@@ -13,13 +13,13 @@ echo "Uploading  my_niryo_robot_description files to robot..."
 sshpass -p robotics scp -r my_niryo_robot/my_niryo_robot_description niryo@$ROS_CONTROLLER_IP:/home/niryo/niryo_robot_ws/src
 
 echo "Updating moveit config of robot ned..."
-sshpass -p robotics scp -r my_niryo_robot/my_niryo_ned_moveit_config niryo@$ROS_MASTER:/home/niryo/niryo_robot_ws/src
+sshpass -p robotics scp -r my_niryo_robot/my_niryo_ned_moveit_config niryo@$ROS_CONTROLLER_IP:/home/niryo/niryo_robot_ws/src
 
 echo "Updating moveit config of robot ned2..."
-sshpass -p robotics scp -r my_niryo_robot/my_niryo_ned2_moveit_config niryo@$ROS_MASTER:/home/niryo/niryo_robot_ws/src
+sshpass -p robotics scp -r my_niryo_robot/my_niryo_ned2_moveit_config niryo@$ROS_CONTROLLER_IP:/home/niryo/niryo_robot_ws/src
 
 echo "Updating moveit config of robot common..."
-sshpass -p robotics scp -r my_niryo_robot/my_niryo_moveit_config niryo@$ROS_MASTER:/home/niryo/niryo_robot_ws/src
+sshpass -p robotics scp -r my_niryo_robot/my_niryo_moveit_config niryo@$ROS_CONTROLLER_IP:/home/niryo/niryo_robot_ws/src
 
 echo "Uploading  my_niryo_robot_bringup files to robot..."
 sshpass -p robotics scp -r my_niryo_robot/my_niryo_robot_bringup niryo@$ROS_CONTROLLER_IP:/home/niryo/niryo_robot_ws/src
