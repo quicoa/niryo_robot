d322b670a2ca60743b1e29eadb16f5febbe25ac0
Quico Augustijn <quico.public@gmail.com>
2023-05-17 10:46:20 +0200

    Improve environment ip setting

--- support/clean_robot.bash
+++ support/clean_robot.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+IP=${ROS_CONTROLLER_IP}
+
 echo "Removing my_niryo_robot files on robot."
-sshpass -p robotics ssh niryo@niryo_pi4 "rm -rf /home/niryo/catkin_ws/src/my_niryo_robot"
+sshpass -p robotics ssh niryo@${IP} "rm -rf /home/niryo/catkin_ws/src/my_niryo_robot"
 echo "Ready"
 
--- support/hotspot_network.bash
+++ support/hotspot_network.bash
@@ -1,7 +1,11 @@
 #!/bin/bash
+
+IP=`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+
 export ROS_CONTROLLER_IP=10.10.10.10
-export ROS_DEVELOPMET_IP=10.10.10.139
+export ROS_DEVELOPMENT_IP=${IP}
+#export ROS_DEVELOPMENT_IP=10.10.10.100
 
 export ROS_MASTER=$ROS_CONTROLLER_IP
 export ROS_MASTER_URI=http://$ROS_CONTROLLER_IP:11311
-export ROS_IP=$ROS_DEVELOPMET_IP
+export ROS_IP=$ROS_DEVELOPMENT_IP
