c1ee908cf76d42ded1779f480bd9918d45234c05
Quico Augustijn <quico.public@gmail.com>
2023-06-21 18:00:59 +0200

    Adjust IP of ROS controller when in Ethernet mode

--- support/ethernet_network.bash
+++ support/ethernet_network.bash
@@ -1,8 +1,8 @@
 IP=`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
 
-export ROS_CONTROLLER_IP=192.254.200.200
+export ROS_CONTROLLER_IP=169.254.200.200
 export ROS_DEVELOPMENT_IP=${IP}
-#export ROS_DEVELOPMENT_IP=192.168.0.100
+#export ROS_DEVELOPMENT_IP=169.254.175.100
 
 export ROS_MASTER=$ROS_CONTROLLER_IP
 export ROS_MASTER_URI=http://$ROS_CONTROLLER_IP:11311
